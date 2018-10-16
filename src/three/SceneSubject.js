394215869ccce95b80fa96cf8c5cb00bf6d032c3
Lucas Swick <lucas@lucastswick.com>
2018-10-16 13:10:00 -0400

    Many solomons seals.

--- src/three/SceneSubject.js
+++ src/three/SceneSubject.js
@@ -12,7 +12,7 @@ const SceneSubject = ({ scene, camera, R, controls }) => {
 
   let solomonsSeal,
     solomonsSealGroup = new Group(),
-    count = 1,
+    count = 6,
     solomonsSealInstances = [];
 
   solomonsSealGroup.position.y = 0;
@@ -39,7 +39,7 @@ const SceneSubject = ({ scene, camera, R, controls }) => {
             R.floatBetween(0.2, 0.4),
             R.floatBetween(0.2, 0.5)
           ),
-          berryCount: 0, //R.intBetween(24, 96),
+          berryCount: R.intBetween(24, 96),
           berryDistanceFromStem: R.floatBetween(0.015, 0.2)
           // berryDisplacement: new Vector2(
           //   R.floatBetween(-0.4, 0.4),
