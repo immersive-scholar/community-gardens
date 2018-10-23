e9de5d5210d2ce4293fa12411bdc8e5e4403e00d
Lucas Swick <lucas@lucastswick.com>
2018-10-23 14:30:49 -0400

    Sky shader.

--- src/art/background/Background.js
+++ src/art/background/Background.js
@@ -1,45 +1,76 @@
 import {
   CubeTextureLoader,
-  SphereGeometry,
+  TetrahedronGeometry,
   MeshPhongMaterial,
+  SphereBufferGeometry,
+  MeshBasicMaterial,
   Mesh,
   DoubleSide,
-  TextureLoader
+  TextureLoader,
+  Sky
 } from "three-full";
 import ColorFactory from "util/ColorFactory";
 
-const Background = () => {
+const Background = ({
+  turbidity = 10,
+  rayleigh = 2,
+  mieCoefficient = 0.005,
+  mieDirectionalG = 0.8,
+  luminance = 1,
+  inclination = 0.5, // elevation / inclination
+  azimuth = 0.25 // Facing front,
+} = {}) => {
   // const loader = new CubeTextureLoader();
   // loader.setPath("/img/skybox/watercolor/");
+  // loader.load(["px.jpg", "nx.jpg", "py.jpg", "ny.jpg", "pz.jpg", "nz.jpg"]);
 
-  // const textureCube = loader.load([
-  //   "px.jpg",
-  //   "nx.jpg",
-  //   "py.jpg",
-  //   "ny.jpg",
-  //   "pz.jpg",
-  //   "nz.jpg"
-  // ]);
+  // const loader = new TextureLoader().load(
+  //   "/img/skybox/watercolor/watercolor-3.jpg"
+  // );
 
-  const loader = new TextureLoader().load(
-    "/img/skybox/watercolor/watercolor-3.jpg"
+  // var geometry = new TetrahedronGeometry(10, 2);
+  // var material = new MeshPhongMaterial({
+  //   color: ColorFactory.getRandomColor(
+  //     ColorFactory.FALL,
+  //     ColorFactory.GREENERY
+  //   ),
+  //   envMap: loader,
+  //   side: DoubleSide,
+  //   emissive: 0x000000,
+  //   specular: 0xffffff,
+  //   shininess: 30
+  // });
+  // const mesh = new Mesh(geometry, material);
+
+  const sunSphere = new Mesh(
+    new SphereBufferGeometry(20000, 16, 8),
+    new MeshBasicMaterial({ color: 0xffffff })
   );
+  sunSphere.position.y = -700000;
+  sunSphere.visible = false;
 
-  var geometry = new SphereGeometry(10, 4, 4, 0, Math.PI);
-  var material = new MeshPhongMaterial({
-    color: ColorFactory.getRandomColor(
-      ColorFactory.FALL,
-      ColorFactory.GREENERY
-    ),
-    // map: loader,
-    side: DoubleSide,
-    emissive: 0x000000,
-    specular: 0xffffff,
-    shininess: 30
-  });
-  const mesh = new Mesh(geometry, material);
+  // Add Sky
+  const sky = new Sky();
+  sky.scale.setScalar(450000);
 
-  return { mesh };
+  var distance = 400000;
+
+  sky.material.lights = false;
+  const uniforms = sky.material.uniforms;
+  uniforms.turbidity.value = turbidity;
+  uniforms.rayleigh.value = rayleigh;
+  uniforms.luminance.value = luminance;
+  uniforms.mieCoefficient.value = mieCoefficient;
+  uniforms.mieDirectionalG.value = mieDirectionalG;
+
+  var theta = Math.PI * (inclination - 0.5);
+  var phi = 2 * Math.PI * (azimuth - 0.5);
+  sunSphere.position.x = distance * Math.cos(phi);
+  sunSphere.position.y = distance * Math.sin(phi) * Math.sin(theta);
+  sunSphere.position.z = -distance * Math.sin(phi) * Math.cos(theta);
+  uniforms.sunPosition.value.copy(sunSphere.position);
+
+  return { mesh: sky };
 };
 
 export default Background;
