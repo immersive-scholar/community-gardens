918f02ac54af98de5f4437b84dce0666cc6b3658
Lucas Swick <lucas@lucastswick.com>
2018-10-25 16:54:51 -0400

    WIP InsecurityCalculator.

--- src/util/InsecurityCalculator.js
+++ src/util/InsecurityCalculator.js
@@ -1,13 +1,29 @@
 class InsecurityCalculator {
   static async parse(data) {
     return new Promise(resolve => {
+      let max = 0,
+        min = 0,
+        minData,
+        maxData;
       for (var i in data) {
         data[i].resourcesIncoming = this.calculateResourcesIncoming(data[i]);
         data[i].energyOutgoing = this.calculateEnergyOutgoing(data[i]);
         data[i].communityFitness = this.calculateCommunityFitness(data[i]);
         data[i].personalScarcity = this.calculatePersonalScarcity(data[i]);
         data[i].health = this.calculateHealth(data[i]);
+
+        if (data[i].health < min) {
+          minData = data[i];
+          min = data[i].health;
+        }
+
+        if (data[i].health > max) {
+          maxData = data[i];
+          max = data[i].health;
+        }
       }
+      console.log("min ", min, minData);
+      console.log("max ", max, maxData);
       resolve(data);
     });
   }
@@ -161,6 +177,10 @@ class InsecurityCalculator {
     const hasChildren = parseInt(d.Children, 10) === 1;
     energyOutgoing += hasChildren ? 4 : 0;
 
+    // if you are a FirstGen, you have to learn everything on your own
+    const isFirstGen = d.FirstGen === "Y";
+    energyOutgoing = isFirstGen ? 2 : 0;
+
     return energyOutgoing;
   }
 
@@ -246,7 +266,145 @@ class InsecurityCalculator {
     personalScarcity += parseInt(d.givemoney, 10) ? -1 : 0;
     personalScarcity += parseInt(d.letstay, 10) ? -1 : 0;
     personalScarcity += parseInt(d.givefood, 10) ? -1 : 0;
-    personalScarcity += parseInt(d.sharemeals, 10) ? -1 : 0;
+
+    // if the user shares meals frequently, it is a sign of
+    // a healthy community
+    personalScarcity += parseInt(d.sharemeals, 10) === 1 ? 1 : 0;
+
+    // "We worried whether our food would run out before we got money to buy more."
+    const worry = parseInt(d.Q36_1, 10);
+    switch (true) {
+      // 1 is often
+      case worry === 1:
+        personalScarcity += 2;
+        break;
+      // 2 is sometimes
+      case worry === 2:
+        personalScarcity += 1;
+        break;
+      default:
+        break;
+    }
+
+    // We worried whether our food would run out before we got money to buy more.
+    const last = parseInt(d.Q36_1, 10);
+    switch (true) {
+      // 1 is often
+      case last === 1:
+        personalScarcity += 4;
+        break;
+      // 2 is sometimes
+      case last === 2:
+        personalScarcity += 2;
+        break;
+      default:
+        break;
+    }
+
+    // The food that we bought just didn’t last, and we didn’t have money to get more.
+    const foodDidntLast = parseInt(d.Q36_2, 10);
+    switch (true) {
+      // 1 is often
+      case foodDidntLast === 1:
+        personalScarcity += 4;
+        break;
+      // 2 is sometimes
+      case foodDidntLast === 2:
+        personalScarcity += 2;
+        break;
+      default:
+        break;
+    }
+
+    // We couldn’t eat balanced meals because we couldn’t afford it.
+    const affordBalancedMeals = parseInt(d.Q36_3, 10);
+    switch (true) {
+      // 1 is often
+      case affordBalancedMeals === 1:
+        personalScarcity += 3;
+        break;
+      // 2 is sometimes
+      case affordBalancedMeals === 2:
+        personalScarcity += 1;
+        break;
+      default:
+        break;
+    }
+
+    // "In the last 30 days, did you or other adults in your household ever cut the size of your meals or skip meals because there wasn’t enough money for food?"
+    const cutMealSize = parseInt(d.Q37_1, 10);
+    switch (true) {
+      // 1 is often
+      case cutMealSize === 1:
+        personalScarcity += 2;
+        break;
+      // 2 is sometimes
+      case cutMealSize === 2:
+        personalScarcity += 1;
+        break;
+      default:
+        break;
+    }
+
+    // "In the last 30 days, did you or other adults in your household ever eat less than you felt you should because there wasn’t enough money for food?"
+    const eatLess = parseInt(d.Q37_3, 10);
+    switch (true) {
+      // 1 is often
+      case eatLess === 1:
+        personalScarcity += 2;
+        break;
+      // 2 is sometimes
+      case eatLess === 2:
+        personalScarcity += 1;
+        break;
+      default:
+        break;
+    }
+
+    // "In the last 30 days, were you or other adults in your household ever hungry but didn’t eat because there wasn’t enough money for food?"
+    const hungry = parseInt(d.Q37_4, 10);
+    switch (true) {
+      // 1 is often
+      case hungry === 1:
+        personalScarcity += 2;
+        break;
+      // 2 is sometimes
+      case hungry === 2:
+        personalScarcity += 1;
+        break;
+      default:
+        break;
+    }
+
+    // "In the last 30 days, did you or other adults in your household lose weight because there wasn’t enough money for food?"
+    const loseWeight = parseInt(d.Q37_5, 10);
+    switch (true) {
+      // 1 is often
+      case loseWeight === 1:
+        personalScarcity += 4;
+        break;
+      // 2 is sometimes
+      case loseWeight === 2:
+        personalScarcity += 2;
+        break;
+      default:
+        break;
+    }
+
+    // "In the last 30 days, did you or other adults in your household ever not eat for a whole day because there wasn't enough money for food?"
+    const notEatForADay = parseInt(d.Q37_6, 10);
+    switch (true) {
+      // 1 is often
+      case notEatForADay === 1:
+        personalScarcity += 6;
+        break;
+      // 2 is sometimes
+      case notEatForADay === 2:
+        personalScarcity += 4;
+        break;
+      default:
+        break;
+    }
 
     return personalScarcity;
   }
