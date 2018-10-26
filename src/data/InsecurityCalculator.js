e8f27de7a09ad72c4bd838d0e6a0ffe049e626ee
Lucas Swick <lucas@lucastswick.com>
2018-10-25 21:13:42 -0400

    Cleanup.

--- src/data/InsecurityCalculator.js
+++ src/data/InsecurityCalculator.js
@@ -46,49 +46,12 @@ class InsecurityCalculator {
       stats.housingInsecurity = HousingInsecurity(data, total);
       stats.pellGrant = PellGrant(data);
 
-      const pellGrant = InsecurityCalculator.count(
-        data,
-        ["Pell"],
-        InsecurityCalculator.COMPARE_NUMERIC_TO_1
-      );
-      stats.pellGrant = {
-        label: `There are ${pellGrant} students who received a Pell Grant.`,
-        count: pellGrant
-      };
-
       console.log("stats ", stats);
 
       data.stats = stats;
       resolve(data);
     });
   }
-
-  static count(data, labels, condition) {
-    let c = 0,
-      label;
-    for (var l in labels) {
-      label = labels[l];
-      for (var i in data) {
-        switch (condition) {
-          // InsecurityCalculator.COMPARE_NUMERIC_TO_1:
-          case 1:
-            c += parseInt(data[i][label], 10) === 1 ? 1 : 0;
-            break;
-          // InsecurityCalculator.COMPARE_NUMERIC_TO_6:
-          case 2:
-            c += parseInt(data[i][label], 10) == 6 ? 1 : 0;
-            break;
-          default:
-            break;
-        }
-      }
-    }
-    return c;
-  }
-
-  // RULES
-  static COMPARE_NUMERIC_TO_1 = 1;
-  static COMPARE_NUMERIC_TO_6 = 2;
 }
 
 export default InsecurityCalculator;
