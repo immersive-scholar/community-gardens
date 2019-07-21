08dd0818066a31f145e818fcafb6c536685abe38
Lucas Swick <lucas@lucastswick.com>
2019-07-21 16:41:23 -0700

    Version bump.

--- src/reducers/version.js
+++ src/reducers/version.js
@@ -1,16 +1,16 @@
 const initialState = {
-  id: "1.1.5",
-  env: process.env.NODE_ENV,
-  releaseDate: "2019-04-02",
+	id: '1.1.6',
+	env: process.env.NODE_ENV,
+	releaseDate: '2019-07-21',
 };
 
 console.log(initialState);
 
 export default function versionReducer(state = initialState, action) {
-  switch (action.type) {
-    default:
-      return {
-        ...state,
-      };
-  }
+	switch (action.type) {
+		default:
+			return {
+				...state,
+			};
+	}
 }
